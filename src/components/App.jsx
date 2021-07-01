8211755eb4f888d77173ededbc0b63168289f6fe
Jordan Mowry <jordan.mowry@gmail.com>
2021-06-30 19:50:48 -0700

    created CreateArea component and added to App.js.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<Footer />
-			{notes.map((note) => (
-				<Note key={note.key} title={note.title} content={note.content} />
-			))}
+			<Note key={1} title="Note title" content="Note content" />
+			<Footer />
 		</div>
 	);
 }
